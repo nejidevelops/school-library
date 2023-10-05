f19d553fe5d2c7877718c1aaac16cce58dbdf991
Newton Ombese <newtonombese1@gmail.com>
2023-10-05 21:11:43 +0300

    Improve my code as requested by the reviewer

--- person.rb
+++ person.rb
@@ -6,7 +6,8 @@ class Person
     @parent_permission = parent_permission
   end
 
-  attr_reader :id, :name, :age
+  attr_reader :id,
+  attr_accessor :name, :age
 
   def can_use_services?
     of_age? || @parent_permission

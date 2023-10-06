854cc8f30a30d563f37d5131abf8e607561f2330
Newton Ombese <newtonombese1@gmail.com>
2023-10-06 14:16:00 +0300

    Add book class

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rentals)
+    @rentals.push(rentals)
+    rentals.book = self
+  end
+end

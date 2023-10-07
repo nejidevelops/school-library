4f54b63fa204633868e011f3760a0344779c6594
Newton Ombese <newtonombese1@gmail.com>
2023-10-07 10:16:21 +0300

    Improve my codes to help my app work

--- student.rb
+++ student.rb
@@ -3,19 +3,17 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(id, age:, classroom:, name: 'Unknown', parent_permission: true)
-    super(id, age: age, name: name, parent_permission: parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def classroom=(classroom)
-    @classroom&.students&.delete(self)
     @classroom = classroom
-    classroom.add_student(self) unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
-    '¯\\(ツ)/¯'
+    '¯\(ツ)/¯'
   end
 end
